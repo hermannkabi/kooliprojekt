02828cb8bcdf32356d192afffe09b182a0896713
JJHellat <kaspar.annus@real.edu.ee>
2023-05-12 11:21:41 +0300

    12 mai tund
    models algus

--- kooliprojekt/app/admin.py
+++ kooliprojekt/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Course,Lesson,LessonCompleted, Question
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(LessonCompleted)
+admin.site.register(Question)
--- kooliprojekt/app/models.py
+++ kooliprojekt/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length = 200)
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    lesson = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
+
+class LessonCompleted(models.Model):
+    lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.lesson
+
+class Question(models.Model):
+    question = models.CharField(max_length=200)
+    def __str__(self):
+        return self.question
